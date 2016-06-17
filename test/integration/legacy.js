d3acdd9443e021b06f532ea9dba2eb9030ecf757
Andrew Sliwinski <andrewsliwinski@acm.org>
2016-06-17 09:49:09 -0400

    Add integration tests for legacy routes

---
+++ test/integration/legacy.js
@@ -0,0 +1,36 @@
+const test = require('tap').test;
+const request = require('request');
+
+test('200', function (t) {
+    request({
+        method: 'POST',
+        uri: 'http://localhost:8444/internalapi/project/new/set',
+        json: require('../fixtures/default.json'),
+        headers: {
+            Cookie: require('../fixtures/users.json').valid
+        }
+    }, function (err, res, body) {
+        t.equal(err, null);
+        t.equal(res.statusCode, 200);
+        t.type(res, 'object');
+        t.type(body, 'object');
+        t.end();
+    });
+});
+
+test('200', function (t) {
+    request({
+        method: 'POST',
+        uri: 'http://localhost:8444/internalapi/project/113799886/set',
+        json: require('../fixtures/default.json'),
+        headers: {
+            Cookie: require('../fixtures/users.json').valid
+        }
+    }, function (err, res, body) {
+        t.equal(err, null);
+        t.equal(res.statusCode, 200);
+        t.type(res, 'object');
+        t.type(body, 'object');
+        t.end();
+    });
+});
