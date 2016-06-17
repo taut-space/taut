1320deabf6ea65972281034c6260433fbf66e458
Andrew Sliwinski <andrewsliwinski@acm.org>
2016-06-17 11:07:14 -0400

    Improve test coverage

--- test/unit/auth.js
+++ test/unit/auth.js
@@ -1,7 +1,42 @@
 const test = require('tap').test;
 const auth = require('../../lib/auth');
+const users = require('../fixtures/users.json');
 
 test('spec', function (t) {
     t.type(auth, 'function');
     t.end();
+});
+
+test('valid cookie', function (t) {
+    const req = {
+        cookies: {
+            scratchsessionsid: users.valid_session
+        }
+    };
+    auth(req, {}, function (err) {
+        t.type(err, 'undefined');
+        t.end();
+    });
+});
+
+test('invalid cookie', function (t) {
+    const req = {
+        cookies: {
+            scratchsessionsid: users.invalid_session
+        }
+    };
+    auth(req, {}, function (err) {
+        t.type(err, 'object');
+        t.end();
+    });
+});
+
+test('no cookie', function (t) {
+    const req = {
+        cookies: {}
+    };
+    auth(req, {}, function (err) {
+        t.type(err, 'object');
+        t.end();
+    });
 });
