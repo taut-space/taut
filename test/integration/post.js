edf4ea4ae030f652c41a04e70e0132899d5437db
Andrew Sliwinski <andrewsliwinski@acm.org>
2016-09-21 18:25:14 -0400

    Handle 'is_copy' query parameter

--- test/integration/post.js
+++ test/integration/post.js
@@ -20,6 +20,23 @@ test('200', function (t) {
     });
 });
 
+test('200', function (t) {
+    request({
+        method: 'POST',
+        uri: 'http://localhost:8444/?title=foobar&is_copy=1',
+        json: require('../fixtures/default.json'),
+        headers: {
+            Cookie: require('../fixtures/users.json').valid
+        }
+    }, function (err, res, body) {
+        t.equal(err, null);
+        t.equal(res.statusCode, 200);
+        t.type(res, 'object');
+        t.type(body, 'object');
+        t.end();
+    });
+});
+
 test('403', function (t) {
     request({
         method: 'POST',
