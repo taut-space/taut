f13cebf3617d32487581b226e1d3e669d23724dc
Colby Gutierrez-Kraybill <colbygk@media.mit.edu>
2018-05-21 18:30:28 -0400

    Update smoke tests for assets

--- test/smoke/get.js
+++ test/smoke/get.js
@@ -3,44 +3,63 @@ const request = require('request');
 
 const host = process.env.SMOKE_HOST || 'http://localhost:8557';
 
+// Get the Scratch Cat via new API
 test('200', function (t) {
     request({
         method: 'GET',
-        uri: host + '/1',
+        uri: host + '/09dc888b0b7df19f70d81588ae73420e.svg',
         json: {}
     }, function (err, res, body) {
         t.equal(err, null);
+        t.equal(res.statusCode, 200);
         t.type(res, 'object');
-        t.type(res.statusCode, 200);
+        t.type(body, 'string');
+        t.end();
+    });
+});
+
+// Get the crossdomain.xml via new API
+test('200', function (t) {
+    request({
+        method: 'GET',
+        uri: host + '/crossdomain.xml',
+        json: {}
+    }, function (err, res, body) {
+        t.equal(err, null);
+        t.equal(res.statusCode, 200);
+        t.type(res, 'object');
+        t.type(body, 'string');
+        t.end();
+    });
+});
+
+// Defaults to health info
+test('200', function (t) {
+    request({
+        method: 'GET',
+        uri: host + '/',
+        json: {}
+    }, function (err, res, body) {
+        t.equal(err, null);
+        t.equal(res.statusCode, 200);
+        t.type(res, 'object');
         t.type(body, 'object');
         t.end();
     });
 });
 
-test('200', function (t) {
-    request({
-        method: 'GET',
-        uri: host + '/1000013887',
-        json: {}
-    }, function (err, res, body) {
-        t.equal(err, null);
-        t.type(res, 'object');
-        t.type(res.statusCode, 200);
-        t.type(body, 'object');
-        t.end();
-    });
-});
-
+// The impossible asset
+// You get Nothing! You lose! Good day sir!
 test('404', function (t) {
     request({
         method: 'GET',
-        uri: host + '/foobar',
+        uri: host + '/0x0x0x0x0x0x0',
         json: {}
     }, function (err, res, body) {
         t.equal(err, null);
         t.type(res, 'object');
         t.type(res.statusCode, 404);
-        t.type(body, 'object');
+        t.type(body, 'string');
         t.end();
     });
 });
--- test/smoke/post.js
+++ test/smoke/post.js
@@ -1,15 +1,16 @@
 const test = require('tap').test;
 const fs = require('fs');
-const path = require('path');
 const request = require('request');
 
-const host = process.env.SMOKE_HOST || 'http://localhost:8444';
+const host = process.env.SMOKE_HOST || 'http://localhost:8557';
 
+// Send a good everything, get a 200 and json
 test('200', function (t) {
+    var image = fs.readFileSync('./test/fixtures/a.png');
     request({
         method: 'POST',
-        uri: host + '/?title=foobar',
-        json: require('../fixtures/default.json'),
+        uri: host + '/eed459aa6ca84d7403768731519d60d3.png',
+        body: image,
         headers: {
             Cookie: require('../fixtures/users.json').valid
         }
@@ -17,83 +18,58 @@ test('200', function (t) {
         t.equal(err, null);
         t.equal(res.statusCode, 200);
         t.type(res, 'object');
-        t.type(body, 'object');
+        t.type(body, 'string');
+        var obj = JSON.parse(body);
+        t.equal(obj.status, 'ok');
+        t.equal(obj['content-name'], 'eed459aa6ca84d7403768731519d60d3.png');
         t.end();
     });
 });
 
-test('200', function (t) {
-    request({
-        method: 'POST',
-        uri: host + '/',
-        json: require('../fixtures/default.json'),
-        qs: {
-            title: 'foobar',
-            is_copy: 1,
-            original_id: 1000014540
-        },
-        headers: {
-            Cookie: require('../fixtures/users.json').valid
-        }
-    }, function (err, res, body) {
-        t.equal(err, null);
-        t.equal(res.statusCode, 200);
-        t.type(res, 'object');
-        t.type(body, 'object');
-        t.end();
-    });
-});
-
+// Send a good everything and invalid credentials, get Forbidden
 test('403', function (t) {
+    var image = fs.readFileSync('./test/fixtures/a.png');
     request({
         method: 'POST',
-        uri: host + '/',
-        json: require('../fixtures/default.json'),
+        uri: host + '/eed459aa6ca84d7403768731519d60d3.png',
+        body: image,
         headers: {
             Cookie: require('../fixtures/users.json').invalid
         }
-    }, function (err, res, body) {
+    }, function (err, res) {
         t.equal(err, null);
         t.equal(res.statusCode, 403);
-        t.type(res, 'object');
-        t.type(body, 'object');
         t.end();
     });
 });
 
-test('413', function (t) {
+// Send a good everything and no credentials, get Forbidden
+test('403', function (t) {
+    var image = fs.readFileSync('./test/fixtures/a.png');
     request({
         method: 'POST',
-        uri: host + '/?title=Untitled',
-        json: require('../fixtures/large.json'),
-        headers: {
-            Cookie: require('../fixtures/users.json').valid
-        }
-    }, function (err, res, body) {
+        uri: host + '/eed459aa6ca84d7403768731519d60d3.png',
+        body: image
+    }, function (err, res) {
         t.equal(err, null);
-        t.equal(res.statusCode, 413);
-        t.type(res, 'object');
-        t.type(body, 'object');
+        t.equal(res.statusCode, 403);
         t.end();
     });
 });
 
-test('500', function (t) {
-    var file = path.resolve(__dirname, '../fixtures/corrupt.json');
-    var json = fs.readFileSync(file).toString('utf8');
-
+// Send a bad hashname, get BadRequest
+test('400', function (t) {
+    var image = fs.readFileSync('./test/fixtures/a.png');
     request({
         method: 'POST',
-        uri: host + '/?title=Untitled',
-        json: json,
+        uri: host + '/whoathisisshort.png',
+        body: image,
         headers: {
             Cookie: require('../fixtures/users.json').valid
         }
-    }, function (err, res, body) {
+    }, function (err, res) {
         t.equal(err, null);
-        t.equal(res.statusCode, 500);
-        t.type(res, 'object');
-        t.type(body, 'undefined');
+        t.equal(res.statusCode, 400);
         t.end();
     });
 });
