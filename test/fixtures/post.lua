e3439ab8c4a40841da42059e199f116c5a2ddeea
Colby Gutierrez-Kraybill <colbygk@media.mit.edu>
2018-06-12 10:37:47 -0400

    Clean up post testing

--- test/fixtures/post.lua
+++ test/fixtures/post.lua
@@ -8,19 +8,5 @@ end
 wrk.method = "POST"
 wrk.body   = readAll('./test/fixtures/a.png')
 wrk.headers["Content-Type"] = "application/octet-stream"
-wrk.headers["Cookie"] = "scratchsessionsid=\".eJxVj09PwzAMxb9LzmvJWvJvN3YEgRBiB7hUTupuoW1StSmVQHx3HGmXSZZivefn_PzL1gXnACOyA3OxXS0i27EG1nRpstX4lpw911IaoclKuCQXY-9zYotzj-1twILrMeRU1jAk7yD5GMqrsZRvOA1X8Xgdpr2RGgpxITtQBhWR3FdWgTVCSqtrWddC2fYAxSm9CvDdp1-64hG_p-Pz-1Y8udOJ1gzx7EPhpwytSy1KYagy4QDhvMI5Y9NHO9Z-kRCb5Ef8iSHLDyPOxHX3glvzQZfd3nWB5UJDghsAvte6EgprVQmogFunuKLXcWmlFMrImv39A-OQb9U:1fO5zn:lSYJ2L4dIwgvHJQ1hOCiu3igWWs\"; scratchcsrftoken=NPGG93pIOxVKWsLy0qMX2KII4ekrrRAj;"
+wrk.headers["Cookie"] = "scratchsessionsid=\".eJxVjz1vgzAQhv8Lc0ttY2PIlgxdGjVSh34t1hkf4AA2BdNIrfrfa0tZInm5e06P3_c321ZcHEyY7bJ1m3EJuAZc1DgO2V0W_IAuEinKktTIStkiZ0JWUDNWIAreEsO12e1Pj93T69vpk8ERq6_vwerzkb67ZvZRM_rOuns7RxOt8krkoo4vAgVb6FWKoKxJlBBCq5rIyMwZXOdVsBP-eJfy7SdcbAMPz3hRH34ZbgU9rH080oYB6JYy3tJC6IICQMmpZJRgYWQVZ5ScktQuVm28H2ySX6IQza1SQxP7p2Bphy7E34P1Lr-CNX_BebwuD9fjv3_FgW7g:1fRJ5j:4fiCKhfV_7K5XZsAg2lbyLmp3Ms\"; scratchcsrftoken=7AF6PrjcHsKhJqUq8sjKJdNoztGUgBhl"
 
-logfile = io.open("wrk.log", "w");
-local cnt = 0;
-
-response = function(status, header, body)
-     logfile:write("status:" .. status .. "\n");
-     cnt = cnt + 1;
-     logfile:write("status:" .. status .. "\n" .. body .. "\n-------------------------------------------------\n");
-end
-
-done = function(summary, latency, requests)
-     logfile:write("------------- SUMMARY -------------\n")
-     print("Response count: ", cnt)
-     logfile.close();
-end
