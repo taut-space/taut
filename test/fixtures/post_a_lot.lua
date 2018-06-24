da35e2c261450b86225f704d792affb2906b9264
Colby Gutierrez-Kraybill <hgranger@unm.edu>
2018-06-23 21:20:43 -0400

    Old session removal

--- test/fixtures/post_a_lot.lua
+++ test/fixtures/post_a_lot.lua
@@ -36,7 +36,7 @@ fileBodies = getBodies(fileNames)
 
 wrk.method = "POST"
 wrk.headers["Content-Type"] = "application/octet-stream"
-wrk.headers["Cookie"] = "scratchsessionsid=\".eJxVjz1vgzAQhv8Lc0ttY2PIlgxdGjVSh34t1hkf4AA2BdNIrfrfa0tZInm5e06P3_c321ZcHEyY7bJ1m3EJuAZc1DgO2V0W_IAuEinKktTIStkiZ0JWUDNWIAreEsO12e1Pj93T69vpk8ERq6_vwerzkb67ZvZRM_rOuns7RxOt8krkoo4vAgVb6FWKoKxJlBBCq5rIyMwZXOdVsBP-eJfy7SdcbAMPz3hRH34ZbgU9rH080oYB6JYy3tJC6IICQMmpZJRgYWQVZ5ScktQuVm28H2ySX6IQza1SQxP7p2Bphy7E34P1Lr-CNX_BebwuD9fjv3_FgW7g:1fRJ5j:4fiCKhfV_7K5XZsAg2lbyLmp3Ms\"; scratchcsrftoken=7AF6PrjcHsKhJqUq8sjKJdNoztGUgBhl"
+wrk.headers["Cookie"] = "FILLME"
 math.randomseed(tonumber(tostring({}):sub(8))+os.time())
 
 request = function()
